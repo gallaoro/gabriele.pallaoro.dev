4cb95cbd00af2aa453b7b8c8311a66f1680dd34a
Gabriele Pallaoro <gabriele.pallaoro@gmail.com>
2021-11-04 04:58:52 +0100

    add stuff

--- pages/api/stuffIMade.js
+++ pages/api/stuffIMade.js
@@ -9,6 +9,11 @@ const stuff = [
     url: 'https://theheadline.vercel.app',
     date: '2021-04-01T12:00:00Z',
   },
+  {
+    title: 'A sugo generator',
+    url: 'https://sugo-generator.vercel.app',
+    date: '2021-11-02T12:00:00Z',
+  },
 ]
 
 export default function handler(req, res) {
